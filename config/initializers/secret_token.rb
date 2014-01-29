c960b0185da36a5f566c49226dad8692b9d687a8
Ruslan Voloshin <rebisall@gmail.com>
2014-01-29 22:30:01 +0200

    initial blank app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Locomotive::Application.config.secret_key_base = '2429d2acaebd144e3c9357d688ebde34922c540692a8a5307b347a834f17f5c43e37eeb709d288b0f75e89f506da0a6cb08f7b85803f166e0cd7410a5a4ab6aa'
